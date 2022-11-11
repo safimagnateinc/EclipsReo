265e06f0f88efde54e26f8d58fa9310c187af163
edrees.safi <edrees.safi@tekschool.us>
2022-11-10 21:08:16 -0500

    commit from Eclips branch

---
+++ src/ClassPractice1.java
@@ -0,0 +1,11 @@
+
+public class ClassPractice1 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("I am going to be successfull Software Developer Engineer at Test");
+
+	}
+
+}
